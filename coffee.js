bd3db7fb19262043d98fa23140a198e3221cbeff
John Stockey <46632935+harujiro@users.noreply.github.com>
2019-04-23 22:57:15 -0700

    Update coffee.js

--- coffee.js
+++ coffee.js
@@ -282,7 +282,7 @@ by the user interacting with the page.
         if (name.value !== "") {
             // Clear name.
             name.value = "";
-            let url = "http://localhost:3000/";
+            let url = "https://coffeemakerservice.herokuapp.com:process.env.PORT/";
             // Build the fetchOptions to be used.
             let fetchOptions = {
                 method : 'POST',
@@ -308,7 +308,7 @@ by the user interacting with the page.
     Clears out the existing combinations and adds in all posted/stored combos.
     **/
     function getCombinations() {
-        let url = "http://localhost:process.env.PORT/";
+        let url = "https://coffeemakerservice.herokuapp.com:process.env.PORT/";
         let postSection = document.getElementById("postsection");
         // Empty out postsection.
         document.getElementById("postsection").innerHTML = "";
